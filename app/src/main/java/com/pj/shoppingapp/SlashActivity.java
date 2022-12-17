ae7d545fa621ceccdc98d76ab0bb37bdb7231716
Trinh <trntrinh79@gmail.com>
2022-12-17 22:40:27 +0700

    Updated 1

---
+++ app/src/main/java/com/pj/shoppingapp/SlashActivity.java
@@ -0,0 +1,30 @@
+package com.pj.shoppingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();}
+            init();
+    }
+    public void init() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SlashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
